e9ac7b483aa59858f434e06f1378c1c4f10b2c26
GodCedric <lygodcedric@gmail.com>
2016-12-19 16:28:55 +0800

    添加最长公共子序列问题

--- DynamicProgramming/DynamicProgramming/DynamicProgramming.cpp
+++ DynamicProgramming/DynamicProgramming/DynamicProgramming.cpp
@@ -2,6 +2,7 @@
 #include<vector>
 #include"CutRod.h"
 #include"MatrixChain.h"
+#include"LCS.h"
 using namespace std;
 
 
@@ -45,6 +46,32 @@ int main(){
 	cout<<endl;
 
 
+	////////最长公共子序列
+	////////最长公共子序列是求两个序列的最长公共子序列
+	////////两个序列为x和y
+	////////其长度分别为m和n
+	////////输出结果即为两序列的最长公共子序列
+	cout<<endl;
+	cout<<"最长公共子序列问题："<<endl;
+	int m = 7;
+	int n = 6;
+	char x[7] = {'A','B','C','B','D','A','B'};
+	char y[6] = {'B','D','C','A','B','A'};
+	cout<<"序列X为：";
+	for(int i=0;i<m;i++)
+		cout<<x[i];
+	cout<<endl;
+	cout<<"序列Y为：";
+	for(int i=0;i<n;i++)
+		cout<<y[i];
+	cout<<endl;
+	int c[8][7] = {0};
+	LCS_length(x,y,m,n,c);
+	cout<<"最长公共子序列长度为：";
+	cout<<c[m][n]<<endl;
+	cout<<"最长公共子序列为：";
+	LCS_print(x,y,m,n,c);
+	cout<<endl;
 
 	
 	return 0;
